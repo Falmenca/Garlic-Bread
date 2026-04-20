096e72727dc5c82ae4a1d12b746d5129593e4048
Kimun001 <57182774+Kimun001@users.noreply.github.com>
2026-04-20 23:45:37 +0200

    mob movement
    simple mob movement and spawner loads in all stats from json

--- garlic/Scripts/Enemy/EnemySpawner.gd
+++ garlic/Scripts/Enemy/EnemySpawner.gd
@@ -5,15 +5,16 @@
 
 extends Node
 
-const SPAWN_RADIUS = 1200
+const SPAWN_RADIUS = 500
 
 @onready var file = FileAccess.open("res://JSON/enemies.json", FileAccess.READ)
 @onready var enemies = JSON.parse_string(file.get_as_text())
-@onready var player = get_node("../Player")
+@onready var player = get_tree().get_first_node_in_group("player")
 @onready var enemy_blueprint = preload("res://Scripts/Enemy/enemy.tscn")
 @onready var enemy_instance
 
 func spawn_enemy(type: String) -> void:
+	## Spawns an enemy of set type around the player
 	var spawn_angle = randf_range(0, TAU)
 	var x_pos = player.position.x+cos(spawn_angle)*SPAWN_RADIUS
 	var y_pos = player.position.y+sin(spawn_angle)*SPAWN_RADIUS
@@ -21,6 +22,9 @@ func spawn_enemy(type: String) -> void:
 	enemy_instance = enemy_blueprint.instantiate()
 	var sprite = enemy_instance.get_node("Sprite2D")
 	sprite.texture = load("res://" + enemies[type]["image"])
+	for stat in enemies[type]:
+		if(stat!="image"):
+			enemy_instance.stats[stat]=enemies[type][stat]
 	add_child(enemy_instance)
 	enemy_instance.set_position(enemy_pos)
 
