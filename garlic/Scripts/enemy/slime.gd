daf31f3b29b7c310e258d1abb5726d499631e33f
Elixiv14 <echuttoory@gmail.com>
2026-04-22 00:35:50 +0400

    slime flip

--- garlic/Scripts/enemy/slime.gd
+++ garlic/Scripts/enemy/slime.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
-var health = 50
-@export var speed = 100
+@onready var RotationOffset: Node2D = $RotationOffset
 
+
+@export var health = 50
+@export var speed = 100
 var PosAttack : Vector2
 var RandDistX = 0
 var RandDistY = 0
+var ChaseDist = 400
 var player 
 
 func _ready():
@@ -17,21 +20,28 @@ func _physics_process(delta: float) -> void:
 	if health < 1:
 		_death()
 	
-	if position.distance_to(player.position)>200:
-		PosAttack= Vector2(player.position.x + RandDistX, player.position.y + RandDistY)
-	else:
-		PosAttack = player.position
-	
-	var direction = (PosAttack - global_position).normalized()
-	velocity = direction * speed
+	if player:
+		_chase()
+
+
 	move_and_slide()
 
 func _death():
 	pass
 	#dies
 
-func _on_pos_timer_timeout() -> void:
-	RandDistX = randf_range(-200,200)
-	RandDistY = randf_range(-200,200)
+func _chase(): # behavior to unstick enemies' balls from each other
+	if position.distance_to(player.position)>ChaseDist:
+		PosAttack= Vector2(player.position.x + RandDistX, player.position.y + RandDistY)
+	else:
+		PosAttack = player.position
+	
+	var direction = (PosAttack - global_position).normalized()
+	velocity = direction * speed 
+	RotationOffset.scale.x = sign(direction.x) #flips sprite/hitbox to direction
+
+func _on_chase_timer_timeout() -> void:
+	RandDistX = randf_range(-ChaseDist,ChaseDist)
+	RandDistY = randf_range(-ChaseDist,ChaseDist)
 
 	print(RandDistY, RandDistX)
