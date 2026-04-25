4b39d4a70400ed3bf369c5bb52537f2ce2ca9236
Elixiv14 <echuttoory@gmail.com>
2026-04-25 23:37:00 +0400

    drill attack

---
+++ garlic/Scripts/Attacks/drill.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
+#Stats
+@export var speed : int = 500
+@export var duration : float = 5
+
+var steer  = 20
+var closest
+var target_angle
+
+func _ready() -> void:
+	z_index = Global.player.z_index-1
+	closest = Global.find_closest_in_group(global_position, "enemies")
+	
+	if closest: 
+		rotation = global_position.angle_to_point(closest.global_position)
+
+
+func _physics_process(delta: float) -> void:
+
+
+	if closest and is_instance_valid(closest):
+		target_angle = global_position.angle_to_point(closest.global_position)
+	else:
+		closest = Global.find_closest_in_group(global_position, "enemies")
+	
+	rotation = lerp_angle(rotation, target_angle, steer * delta)
+	position += transform.x * speed * delta
+	
+	_expire(delta)
+
+func _expire(delta):
+	duration -= delta
+	if duration < 0.0:
+		anim.play("expire")
