8891d13bae76532fbf3ee6d2a8c2c662fcf31a07
Enrique Delacruz <enriquedelacruz2436@gmail.com>
2026-04-26 03:16:24 -0700

    Added rest of the helper functions to edit all Player Stats

--- garlic/Scripts/Utilities/Player_Stats.gd
+++ garlic/Scripts/Utilities/Player_Stats.gd
@@ -18,10 +18,26 @@ var pStats = {}
 func reset_pStats():
 	pStats = pStats_default.duplicate(true)
 
+func set_pname(value: String):
+	pStats.pname = value
+	pStats.changed.emit()
+
 func set_health(value: int):
 	pStats.health = value
 	pStats.changed.emit()
 
+func set_max_health(value: int):
+	pStats.max_health = value
+	pStats.changed.emit()
+
 func set_level(value: int):
 	pStats.level = value
+	pStats_changed.emit()
+
+func set_strength(value: int):
+	pStats.strength = value
+	pStats_changed.emit()
+
+func set_defense(value: int):
+	pStats.defense = value
 	pStats_changed.emit()
